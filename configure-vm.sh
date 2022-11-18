7179f5dce8a8a5ae29b5d1d607635e6992b92747
Lovell <hydroxyzine@metacity.email>
2022-11-18 18:37:06 +0700

    add docker configs

--- configure-vm.sh
+++ configure-vm.sh
@@ -4,5 +4,9 @@
 sudo apt update -y
 sudo apt install net-tools --no-install-recommends
 
+# allow non-root user to run docker
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
+
 # Enable IP Forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
